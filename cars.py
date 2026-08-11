f2951bc3706973a996a247548398c8569fb570b4
deshmukhsiddhi18 <deshmukhsiddhi042@gmail.com>
2026-08-11 19:43:43 +0530

    Update cars.py

--- cars.py
+++ cars.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 
 from db import get_connection
-db = get_connection()
+
 cars_bp = Blueprint("cars",__name__)
 @cars_bp.route("/cars")
 def cars():
-    
+    db = get_connection()
     cursor = db.cursor(dictionary=True)
     
     cursor.execute("SELECT * FROM cars WHERE available = TRUE")
