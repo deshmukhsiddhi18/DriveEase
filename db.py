d65737f556ed8ea805536fc0be09b95f38cb396c
deshmukhsiddhi18 <deshmukhsiddhi042@gmail.com>
2026-08-11 19:55:57 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,14 +6,15 @@ print("PORT:", os.getenv("MYSQLPORT"))
 print("USER:", os.getenv("MYSQLUSER"))
 print("DB:", os.getenv("MYSQLDATABASE"))
 
+
 def get_connection():
-    return mysql.connector.connect(
+    conn = mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQL_DATABASE"),   # <-- changed
-       port = int(os.getenv("MYSQLPORT", 3306))
-       
+        port=int(os.getenv("MYSQLPORT", "3306"))
     )
-   conn.database = "railway"
-   return conn
+
+    conn.database = "railway"
+
+    return conn
