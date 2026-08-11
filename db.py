41c9036cc2cd2a677b053f6a6d8d00be3c074caf
deshmukhsiddhi18 <deshmukhsiddhi042@gmail.com>
2026-08-11 19:11:04 +0530

    Update db.py

--- db.py
+++ db.py
@@ -14,3 +14,11 @@ def get_connection():
         database=os.getenv("MYSQL_DATABASE"),   # <-- changed
        port = int(os.getenv("MYSQLPORT", 3306))
     )
+
+try:
+    conn = get_connection()
+    print("DATABASE CONNECTION: SUCCESS")
+    conn.close()
+except Exception as e:
+    print("DATABASE CONNECTION: FAILED")
+    print("ERROR:", e)
