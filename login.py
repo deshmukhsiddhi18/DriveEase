1881c4089dfc478b6475f9a452a410f0f65fd0b5
deshmukhsiddhi18 <deshmukhsiddhi042@gmail.com>
2026-08-12 11:43:53 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,13 @@
-from flask import Blueprint, render_template,request,session,redirect,url_for
+from flask import Blueprint, render_template, request, session, redirect, url_for
 import mysql.connector
-
-
 
 from db import get_connection
 
-db = get_connection()
+login_bp = Blueprint("login", __name__)
 
-login_bp = Blueprint("login" , __name__)
-@login_bp.route("/login",methods=["GET","POST"])
+@login_bp.route("/login", methods=["GET", "POST"])
 def login():
-   
+    db = get_connection()
 
     if request.method == "POST":
 
@@ -19,27 +16,34 @@ def login():
 
         cursor = db.cursor(dictionary=True)
 
-        cursor.execute("SELECT * FROM users WHERE email = %s",(email,))
-        
-      
+        cursor.execute(
+            "SELECT * FROM users WHERE email = %s",
+            (email,)
+        )
 
         user = cursor.fetchone()
 
         if not user:
             cursor.close()
+            db.close()
             return render_template(
                 "login.html",
-                error="Email is not registered. Please register first.")
+                error="Email is not registered. Please register first."
+            )
 
         if user["password"] != password:
             cursor.close()
-            return render_template("login.html",error="Incorrect password.")
-
+            db.close()
+            return render_template(
+                "login.html",
+                error="Incorrect password."
+            )
 
         session["user_id"] = user["id"]
+
         cursor.close()
-            
+        db.close()
 
         return redirect(url_for("index"))
-        
+
     return render_template("login.html")
