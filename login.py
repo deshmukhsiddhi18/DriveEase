63d493989fa71a02944fd0180b3ded064dfbc75f
deshmukhsiddhi18 <deshmukhsiddhi042@gmail.com>
2026-08-12 11:55:21 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,14 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
-import mysql.connector
-
 from db import get_connection
 
 login_bp = Blueprint("login", __name__)
+
 
 @login_bp.route("/login", methods=["GET", "POST"])
 def login():
     db = get_connection()
 
     if request.method == "POST":
-
         email = request.form["email"]
         password = request.form["password"]
 
@@ -26,6 +24,7 @@ def login():
         if not user:
             cursor.close()
             db.close()
+
             return render_template(
                 "login.html",
                 error="Email is not registered. Please register first."
@@ -34,6 +33,7 @@ def login():
         if user["password"] != password:
             cursor.close()
             db.close()
+
             return render_template(
                 "login.html",
                 error="Incorrect password."
@@ -44,6 +44,6 @@ def login():
         cursor.close()
         db.close()
 
-            return redirect(url_for("index"))
+        return redirect(url_for("index"))
 
     return render_template("login.html")
